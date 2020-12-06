f4f53dfec918d32fe459a24530a6fbf9bc86b0cd
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2020-12-06 16:30:15 -0500

    simple-smooth-circle

--- image.glsl
+++ image.glsl
@@ -1,40 +1,31 @@
-#version 120
 uniform sampler2D iChannel0;
 uniform sampler2D iChannel1;
 uniform vec4 iMouse;
 uniform vec2 iResolution;
 uniform float iTime;
 
-#define PI 3.14159265358979323846264
-#define  E 2.71828182845904523536028
-
-vec4 multiply(vec4 a, vec4 b)
+//uv,circle pos , circle radius,circle edge feather
+float circle(vec2 uv,vec2 pos,float radius,float feather)
 {
-    return a * b;
+    vec2 uvDist=uv-pos;
+    return 1.0-smoothstep(radius-feather,radius+feather, length(uvDist));
 }
 
-vec4 screen(vec4 a, vec4 b)
+float sdCircle( vec2 p, float r )
 {
-
-    return 1 - ( (1 - a) * (1 - b) );
-
+  return length(p) - r;
 }
 
 void main(void)
 {
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = gl_FragCoord.xy/iResolution.y;
 
-    vec2 uv = gl_FragCoord.xy/iResolution.xy;
-
-    float cycle = PI*2.0/3.0;
-
-    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0, cycle, cycle*2.0));
-
-    vec4 tex0 = texture2D(iChannel0, uv);
-    vec4 tex1 = texture2D(iChannel1, uv);
-    vec4 color = vec4(col, 1.0);
-        
-    if (tex1.r < .5 && tex1.g < .5 && tex1.b < .5)
-        gl_FragColor = screen(tex0, color);
-    else
-        gl_FragColor = multiply(tex1, color);
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+	col+=circle(uv,vec2(0.5,0.5),0.2,0.002);
+    
+    col*=vec3(sdCircle( uv-vec2(0.7,0.8), 0.1 ));
+    // Output to screen
+    gl_FragColor = vec4(col,1.0);
 }
