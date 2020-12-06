ca25bdd8f4808a25ec79282f42224efaa35dcde7
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2020-12-06 15:34:16 -0500

    Add fog effect

--- image.glsl
+++ image.glsl
@@ -119,6 +119,40 @@ float NetLayer(vec2 st, float n, float t) {
     return m;
 }
 
+uniform vec4 color = vec4(0.35, 0.48, 0.95,1);
+uniform int OCTAVES = 4;
+
+float rand(vec2 coord){
+	return fract(sin(dot(coord, vec2(56, 78)) * 1000.0) * 1000.0);
+}
+
+float noise(vec2 coord){
+	vec2 i = floor(coord);
+	vec2 f = fract(coord);
+
+	// 4 corners of a rectangle surrounding our point
+	float a = rand(i);
+	float b = rand(i + vec2(1.0, 0.0));
+	float c = rand(i + vec2(0.0, 1.0));
+	float d = rand(i + vec2(1.0, 1.0));
+
+	vec2 cubic = f * f * (3.0 - 2.0 * f);
+
+	return mix(a, b, cubic.x) + (c - a) * cubic.y * (1.0 - cubic.x) + (d - b) * cubic.x * cubic.y;
+}
+
+float fbm(vec2 coord){
+	float value = 0.0;
+	float scale = 0.5;
+
+	for(int i = 0; i < OCTAVES; i++){
+		value += noise(coord) * scale;
+		coord *= 2.0;
+		scale *= 0.5;
+	}
+	return value;
+}
+
 void main(void)
 {
     vec2 uv = (gl_FragCoord.xy-iResolution.xy*.5)/iResolution.y;
@@ -165,4 +199,13 @@ void main(void)
     #endif
     
     gl_FragColor = vec4(col,1);
+    
+    vec2 coord = uv * 2.0;
+
+    vec2 motion = vec2( fbm(coord + vec2(iTime * -0.5, iTime * 0.5)) );
+
+    float final = fbm(coord + motion);
+        
+    gl_FragColor = mix(gl_FragColor, vec4(baseCol, 1), final);
+
 }
