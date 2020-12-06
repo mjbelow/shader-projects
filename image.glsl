ff0647e4d3c61ee7944f5e3f3e3b4108d1cfe048
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2020-12-06 15:55:40 -0500

    just-snow

--- image.glsl
+++ image.glsl
@@ -1,40 +1,49 @@
-#version 120
 uniform sampler2D iChannel0;
 uniform sampler2D iChannel1;
 uniform vec4 iMouse;
 uniform vec2 iResolution;
 uniform float iTime;
 
-#define PI 3.14159265358979323846264
-#define  E 2.71828182845904523536028
+// Copyright (c) 2013 Andrew Baldwin (twitter: baldand, www: http://thndl.com)
+// License = Attribution-NonCommercial-ShareAlike (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US)
 
-vec4 multiply(vec4 a, vec4 b)
-{
-    return a * b;
-}
+// "Just snow"
+// Simple (but not cheap) snow made from multiple parallax layers with randomly positioned 
+// flakes and directions. Also includes a DoF effect. Pan around with mouse.
 
-vec4 screen(vec4 a, vec4 b)
-{
+#define LIGHT_SNOW // Comment this out for a blizzard
 
-    return 1 - ( (1 - a) * (1 - b) );
-
-}
+#ifdef LIGHT_SNOW
+	#define LAYERS 50
+	#define DEPTH .5
+	#define WIDTH .3
+	#define SPEED .6
+#else // BLIZZARD
+	#define LAYERS 200
+	#define DEPTH .1
+	#define WIDTH .8
+	#define SPEED 1.5
+#endif
 
 void main(void)
 {
-
-    vec2 uv = gl_FragCoord.xy/iResolution.xy;
-
-    float cycle = PI*2.0/3.0;
-
-    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0, cycle, cycle*2.0));
-
-    vec4 tex0 = texture2D(iChannel0, uv);
-    vec4 tex1 = texture2D(iChannel1, uv);
-    vec4 color = vec4(col, 1.0);
-        
-    if (tex1.r < .5 && tex1.g < .5 && tex1.b < .5)
-        gl_FragColor = screen(tex0, color);
-    else
-        gl_FragColor = multiply(tex1, color);
+	const mat3 p = mat3(13.323122,23.5112,21.71123,21.1212,28.7312,11.9312,21.8112,14.7212,61.3934);
+	vec2 uv = iMouse.xy/iResolution.xy + vec2(1.,iResolution.y/iResolution.x)*gl_FragCoord.xy / iResolution.xy;
+	vec3 acc = vec3(0.0);
+	float dof = 5.*sin(iTime*.1);
+	for (int i=0;i<LAYERS;i++) {
+		float fi = float(i);
+		vec2 q = uv*(1.+fi*DEPTH);
+		q += vec2(q.y*(WIDTH*mod(fi*7.238917,1.)-WIDTH*.5),SPEED*iTime/(1.+fi*DEPTH*.03));
+		vec3 n = vec3(floor(q),31.189+fi);
+		vec3 m = floor(n)*.00001 + fract(n);
+		vec3 mp = (31415.9+m)/fract(p*m);
+		vec3 r = fract(mp);
+		vec2 s = abs(mod(q,1.)-.5+.9*r.xy-.45);
+		s += .01*abs(2.*fract(10.*q.yx)-1.); 
+		float d = .6*max(s.x-s.y,s.x+s.y)+max(s.x,s.y)-.01;
+		float edge = .005+.05*min(.5*abs(fi-5.-dof),1.);
+		acc += vec3(smoothstep(edge,-edge,d)*(r.x/(1.+.02*fi*DEPTH)));
+	}
+	gl_FragColor = vec4(vec3(acc),1.0);
 }
