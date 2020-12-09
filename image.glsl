6072f199f891041f8a78825a0abd7c80ae2baa66
Matthew Below <matthew.below@altardstate.com>
2020-12-09 14:19:28 -0500

    sunset

--- image.glsl
+++ image.glsl
@@ -1,40 +1,38 @@
-#version 120
 uniform sampler2D iChannel0;
 uniform sampler2D iChannel1;
 uniform vec4 iMouse;
 uniform vec2 iResolution;
 uniform float iTime;
 
-#define PI 3.14159265358979323846264
-#define  E 2.71828182845904523536028
-
-vec4 multiply(vec4 a, vec4 b)
+float cr(float a, float b)
 {
-    return a * b;
-}
-
-vec4 screen(vec4 a, vec4 b)
-{
-
-    return 1 - ( (1 - a) * (1 - b) );
-
+    //return a;
+    return floor(a / b) * b;
 }
 
 void main(void)
 {
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = (gl_FragCoord.xy)/iResolution.xy;
 
-    vec2 uv = gl_FragCoord.xy/iResolution.xy;
+    uv.x = (gl_FragCoord.x - iResolution.x*.5) / iResolution.x;
+    uv.y = (gl_FragCoord.y) / iResolution.y;
+    
+    //uv.y = uv.y - 1.;
+    
+    //uv = vec2(length(uv), atan(uv.x,uv.y));
+    
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5*cos(iTime+cr(atan(uv.y, uv.x),.2)+vec3(0,2,4));
 
-    float cycle = PI*2.0/3.0;
-
-    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0, cycle, cycle*2.0));
-
-    vec4 tex0 = texture2D(iChannel0, uv);
-    vec4 tex1 = texture2D(iChannel1, uv);
-    vec4 color = vec4(col, 1.0);
-        
-    if (tex1.r < .5 && tex1.g < .5 && tex1.b < .5)
-        gl_FragColor = screen(tex0, color);
-    else
-        gl_FragColor = multiply(tex1, color);
+    mat3 avg;
+    avg[0] = vec3(.393, .769, .189);
+    avg[1] = vec3(.349, .686, .168);
+    avg[2] = vec3(.272, .534, .131);
+    
+    
+    //col *= avg;
+    
+    // Output to screen
+    gl_FragColor = vec4(col,1.0);
 }
