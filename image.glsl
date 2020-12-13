34270c1555acf9011522b98338f5fc0997df7646
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2020-12-13 11:17:17 -0500

    pixel-waves

--- image.glsl
+++ image.glsl
@@ -5,36 +5,54 @@ uniform vec4 iMouse;
 uniform vec2 iResolution;
 uniform float iTime;
 
-#define PI 3.14159265358979323846264
-#define  E 2.71828182845904523536028
-
-vec4 multiply(vec4 a, vec4 b)
+float customRound(float f, float c)
 {
-    return a * b;
+    return floor(f / c) * c;
 }
 
-vec4 screen(vec4 a, vec4 b)
+vec2 customRound(vec2 f, float c)
 {
-
-    return 1 - ( (1 - a) * (1 - b) );
-
+    return vec2(customRound(f.x, c), customRound(f.y, c));
 }
 
 void main(void)
 {
-
-    vec2 uv = gl_FragCoord.xy/iResolution.xy;
-
-    float cycle = PI*2.0/3.0;
-
-    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0, cycle, cycle*2.0));
-
-    vec4 tex0 = texture2D(iChannel0, uv);
-    vec4 tex1 = texture2D(iChannel1, uv);
-    vec4 color = vec4(col, 1.0);
-        
-    if (tex1.r < .5 && tex1.g < .5 && tex1.b < .5)
-        gl_FragColor = screen(tex0, color);
+    /*
+    // Normalized pixel coordinates (from 0 to 1)
+    
+    vec2 coord = gl_FragCoord.xy;
+    
+    if (int(gl_FragCoord.y) % 100 < 50)
+        coord.x += sin(iTime*2.)*20.0;
     else
-        gl_FragColor = multiply(tex1, color);
+    	coord.x += sin(iTime*2.+.2)*20.0;
+    
+    vec2 uv = (coord)/iResolution.xy;
+
+    //if(((int)iResolution.y) % 6 == 0)
+        //uv /= 3.;
+    
+    //uv.x += 36.;
+    
+    //int d = int(gl_FragCoord.y);
+    //if (d % 6 == 0)
+        //uv /= 4.;
+    
+    // sample texture and output to screen
+    gl_FragColor = texture2D(iChannel0, uv);
+    */
+    bool water = false;
+    vec2 uv = gl_FragCoord.xy / iResolution.xy;
+    uv.y *= -1.;
+
+    if (uv.x > .4 && uv.x < .6)
+    {
+    	uv.x += 0.02*cos(-iTime*4.+uv.y*64.);
+        water = true;
+    }
+    
+    gl_FragColor = texture2D(iChannel0,uv);
+    
+    if (water)
+        gl_FragColor.rg *= vec2(.25,.5);
 }
