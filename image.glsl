9ebea661a7e2651febfde8240d5ac66edc0863a4
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2020-12-18 16:11:17 -0500

    smoothstep

--- image.glsl
+++ image.glsl
@@ -1,40 +1,59 @@
-#version 120
 uniform sampler2D iChannel0;
 uniform sampler2D iChannel1;
 uniform vec4 iMouse;
 uniform vec2 iResolution;
 uniform float iTime;
 
-#define PI 3.14159265358979323846264
-#define  E 2.71828182845904523536028
+#define S(a, b, d) smoothstep(a, b, d)
+#define s(d, a) step(d, a)
 
-vec4 multiply(vec4 a, vec4 b)
+float DistLine(vec2 p, vec2 a, vec2 b)
 {
-    return a * b;
+ 	vec2 pa = p-a;
+    vec2 ba = b-a;
+    float t = clamp(dot(pa,ba)/dot(ba,ba), 0., 1.);
+    
+    return length(pa - ba * t);
 }
 
-vec4 screen(vec4 a, vec4 b)
+
+float inverse_smoothstep( float x )
 {
+    return 0.5 - sin(asin(1.0-2.0*x)/3.0);
+}
 
-    return 1 - ( (1 - a) * (1 - b) );
-
+float Line(vec2 p, vec2 a, vec2 b) {
+    float d = DistLine(p, a, b);
+    float m = S(.05, .004, d);
+    m = s(d, .05);
+    
+    //m = inverse_smoothstep(m);
+    return clamp(m, 0.0, 1.0);
 }
 
 void main(void)
 {
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = (gl_FragCoord.xy - .5 * iResolution.xy) / iResolution.y;
+    //vec2 uv = gl_FragCoord.xy/iResolution.xy;
+    
+    vec2 a = vec2(-.5,0);
+    vec2 b = vec2(.0, 0);
+    vec2 c = vec2(.0, .5);
+    
+    float l1 = Line(uv, a, b);
+    float l2 = Line(uv, b, c);
+    float l3 = Line(uv, c, a);
 
-    vec2 uv = gl_FragCoord.xy/iResolution.xy;
 
-    float cycle = PI*2.0/3.0;
-
-    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0, cycle, cycle*2.0));
-
-    vec4 tex0 = texture2D(iChannel0, uv);
-    vec4 tex1 = texture2D(iChannel1, uv);
-    vec4 color = vec4(col, 1.0);
-        
-    if (tex1.r < .5 && tex1.g < .5 && tex1.b < .5)
-        gl_FragColor = screen(tex0, color);
+    vec3 col;
+    
+    // Time varying pixel color
+    if (l1 >= .01 || l2 >= .02 || l3 == 1.)
+    	col = vec3(.5*.5+cos(vec3(iTime) + vec3(0,2,4)));
     else
-        gl_FragColor = multiply(tex1, color);
+        col = vec3(0);
+
+    // Output to screen
+    gl_FragColor = vec4(col,1.0);
 }
