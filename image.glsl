970fad4726618bbd0b2c3e18e2717b4a2c9ae210
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2020-12-23 17:05:12 -0500

    bayer-dither

--- image.glsl
+++ image.glsl
@@ -1,40 +1,29 @@
-#version 120
 uniform sampler2D iChannel0;
 uniform sampler2D iChannel1;
 uniform vec4 iMouse;
 uniform vec2 iResolution;
 uniform float iTime;
 
-#define PI 3.14159265358979323846264
-#define  E 2.71828182845904523536028
+// inspired by https://www.shadertoy.com/view/lllSRj
 
-vec4 multiply(vec4 a, vec4 b)
-{
-    return a * b;
-}
 
-vec4 screen(vec4 a, vec4 b)
-{
+void main(void) { 
 
-    return 1 - ( (1 - a) * (1 - b) );
+// --- color version (base = 110 chars)
+    gl_FragColor = step(texture2D(iChannel0, gl_FragCoord.xy/8.).r, texture2D(iChannel1,gl_FragCoord.xy/iResolution.xy));
 
-}
+    
+    
+// --- color version + gamma correction ( + 15 chars):     
+//   gl_FragColor += step(pow(texture2D(iChannel0, gl_FragCoord.xy/8.),vec4(.45)), texture2D(iChannel1,gl_FragCoord.xy/iResolution.xy));
 
-void main(void)
-{
+    
+    
+// --- B&W version ( base + 1 chars): 
+// texture2D(iChannel0, gl_FragCoord.xy/8.).r < texture2D(iChannel1,gl_FragCoord.xy/iResolution.xy).r  ? gl_FragColor++ : gl_FragColor;
+    
 
-    vec2 uv = gl_FragCoord.xy/iResolution.xy;
-
-    float cycle = PI*2.0/3.0;
-
-    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0, cycle, cycle*2.0));
-
-    vec4 tex0 = texture2D(iChannel0, uv);
-    vec4 tex1 = texture2D(iChannel1, uv);
-    vec4 color = vec4(col, 1.0);
-        
-    if (tex1.r < .5 && tex1.g < .5 && tex1.b < .5)
-        gl_FragColor = screen(tex0, color);
-    else
-        gl_FragColor = multiply(tex1, color);
+    
+// --- B&W version + gamma correction ( + 9 chars): 
+// pow(texture2D(iChannel0, gl_FragCoord.xy/8.).r, .45) < texture2D(iChannel1,gl_FragCoord.xy/iResolution.xy).r  ? gl_FragColor++ : gl_FragColor;
 }
