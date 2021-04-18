f919a7fbb5e20b9c48763eb4568713829e7f166a
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2021-04-18 13:08:30 -0400

    Incorporate cube maps

--- shadertoy port.sh
+++ shadertoy port.sh
@@ -6,6 +6,12 @@ out vec4 FragColor;
 
 uniform sampler2D iChannel0;
 uniform sampler2D iChannel1;
+uniform sampler2D iChannel2;
+uniform sampler2D iChannel3;
+uniform samplerCube iChannel4;
+uniform samplerCube iChannel5;
+uniform samplerCube iChannel6;
+uniform samplerCube iChannel7;
 uniform vec4 iMouse;
 uniform vec2 iResolution;
 uniform float iTime;
