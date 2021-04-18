e396b7d354c020c1e4dd191823adf68eb8ee8e7c
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2021-04-18 14:31:41 -0400

    Create new demo using cube map

--- image.glsl
+++ image.glsl
@@ -3,26 +3,118 @@ out vec4 FragColor;
 
 uniform sampler2D iChannel0;
 uniform sampler2D iChannel1;
+uniform sampler2D iChannel2;
+uniform sampler2D iChannel3;
+uniform samplerCube iChannel4;
+uniform samplerCube iChannel5;
+uniform samplerCube iChannel6;
+uniform samplerCube iChannel7;
 uniform vec4 iMouse;
 uniform vec2 iResolution;
 uniform float iTime;
 
 
-#define PI 3.14159265358979323846264
-#define  E 2.71828182845904523536028
+#define PI acos(-1.)
+#define PI2 PI * 2.
+#define E 2.71828182845904523536028
 
-vec4 multiply(vec4 a, vec4 b)
-{
+/**
+ * Rotation matrix around the X axis.
+ */
+mat3 rotateX(float theta) {
+    float c = cos(theta);
+    float s = sin(theta);
+    return mat3(
+        vec3(1, 0, 0),
+        vec3(0, c, -s),
+        vec3(0, s, c)
+    );
+}
+
+/**
+ * Rotation matrix around the Y axis.
+ */
+mat3 rotateY(float theta) {
+    float c = cos(theta);
+    float s = sin(theta);
+    return mat3(
+        vec3(c, 0, s),
+        vec3(0, 1, 0),
+        vec3(-s, 0, c)
+    );
+}
+
+/**
+ * Rotation matrix around the Z axis.
+ */
+mat3 rotateZ(float theta) {
+    float c = cos(theta);
+    float s = sin(theta);
+    return mat3(
+        vec3(c, -s, 0),
+        vec3(s, c, 0),
+        vec3(0, 0, 1)
+    );
+}
+
+/**
+ * Constructive solid geometry intersection operation on SDF-calculated distances.
+ */
+float intersectSDF(float distA, float distB) {
+    return max(distA, distB);
+}
+
+/**
+ * Constructive solid geometry union operation on SDF-calculated distances.
+ */
+float unionSDF(float distA, float distB) {
+    return min(distA, distB);
+}
+
+/**
+ * Constructive solid geometry difference operation on SDF-calculated distances.
+ */
+float differenceSDF(float distA, float distB) {
+    return max(distA, -distB);
+}
+
+/**
+ * Return the normalized direction to march in from the eye point for a single pixel.
+ *
+ * fieldOfView: vertical field of view in degrees
+ * size: resolution of the output image
+ * fragCoord: the x,y coordinate of the pixel in the output image
+ */
+vec3 rayDirection(float fieldOfView, vec2 size, vec2 fragCoord) {
+    vec2 xy = fragCoord - size / 2.0;
+    float z = size.y / tan(radians(fieldOfView) / 2.0);
+    return normalize(vec3(xy, -z));
+}
+
+/**
+ * Return a transform matrix that will transform a ray from view space
+ * to world coordinates, given the eye point, the camera target, and an up vector.
+ *
+ * This assumes that the center of the camera is aligned with the negative z axis in
+ * view space when calculating the ray marching direction. See rayDirection.
+ */
+mat3 viewMatrix(vec3 eye, vec3 center, vec3 up) {
+    // Based on gluLookAt man page
+    vec3 f = normalize(center - eye);
+    vec3 s = normalize(cross(f, up));
+    vec3 u = cross(s, f);
+    return mat3(s, u, -f);
+}
+
+//Multiply
+vec4 multiply(vec4 a, vec4 b){
     return a * b;
 }
 
-vec4 screen(vec4 a, vec4 b)
-{
-
+//Screen
+vec4 screen(vec4 a, vec4 b){
     return 1 - ( (1 - a) * (1 - b) );
-
 }
-
 
 //Color Burn
 vec4 colorBurn (vec4 target, vec4 blend){
@@ -73,7 +165,7 @@ vec4 hardLight (vec4 target, vec4 blend){
 
 //Vivid Light
 vec4 vividLight (vec4 target, vec4 blend){
-     vec4 temp;
+    vec4 temp;
     temp.x = (blend.x > 0.5) ? (1.0-(1.0-target.x)/(2.0*(blend.x-0.5))) : (target.x / (1.0-2.0*blend.x));
     temp.y = (blend.y > 0.5) ? (1.0-(1.0-target.y)/(2.0*(blend.y-0.5))) : (target.y / (1.0-2.0*blend.y));
     temp.z = (blend.z > 0.5) ? (1.0-(1.0-target.z)/(2.0*(blend.z-0.5))) : (target.z / (1.0-2.0*blend.z));
@@ -91,7 +183,7 @@ vec4 linearLight (vec4 target, vec4 blend){
 
 //Pin Light
 vec4 pinLight (vec4 target, vec4 blend){
-     vec4 temp;
+    vec4 temp;
     temp.x = (blend.x > 0.5) ? (max (target.x, 2.0*(blend.x-0.5))) : (min(target.x, 2.0*blend.x));
     temp.y = (blend.y > 0.5) ? (max (target.y, 2.0*(blend.y-0.5))) : (min(target.y, 2.0*blend.y));
     temp.z = (blend.z > 0.5) ? (max (target.z, 2.0*(blend.z-0.5))) : (min(target.z, 2.0*blend.z));
@@ -100,36 +192,43 @@ vec4 pinLight (vec4 target, vec4 blend){
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
+    vec3 viewDir = rayDirection(90.0, iResolution.xy, fragCoord);
+    vec3 eye = vec3(8.0, 5.0 * sin(0.2 * iTime), 7.0);
+
+    float mx=iMouse.x/iResolution.x*PI*2.0;
+    float my=iMouse.y/iResolution.y*3.14 + PI/2.0;
+    eye = vec3(cos(my)*cos(mx),sin(my),cos(my)*sin(mx));//*7.;
+
+    mat3 viewToWorld = viewMatrix(eye, vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0));
+
+    vec3 worldDir = viewToWorld * viewDir;
+
+    vec4 tex = texture(iChannel4, worldDir);
+
+    // The closest point on the surface to the eyepoint along the view ray
+    vec3 p = eye + worldDir;
 
     vec2 uv = gl_FragCoord.xy/iResolution.xy;
 
-    float cycle = PI*2.0/3.0;
-
-    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0, cycle, cycle*2.0));
-
-    vec4 tex0 = texture2D(iChannel0, uv);
-    vec4 tex1 = texture2D(iChannel1, uv);
-    vec4 color = vec4(col, 1.0);
-
-    if (tex1.r < .5 && tex1.g < .5 && tex1.b < .5)
-        fragColor = screen(tex0, color);
-    else
-        fragColor = multiply(tex1, color);
-
-    float amt = 3.;
-    float lines = mod(gl_FragCoord.y, amt);
-    float inter = .2;
+    float amt = .125;
+    float ratio = 4.;
+    float lines_x = mod(p.x, amt);
+    float lines_y = mod(p.y, amt);
+    float lines_z = mod(p.z, amt);
 
     float line_color = 0;
 
-    if (lines < amt / 2.)
-        line_color = 0.3;
+    if (lines_x < amt / pow(2.,ratio) || lines_y < amt / pow(2.,ratio) || lines_z < amt / pow(2.,ratio))
+        line_color = .3;
     else
-        line_color = .7;
+        line_color = .5;
 
-    // fragColor = (fragColor * (1.-inter)) + (vec4(line_color) * inter);
+    float inter = .5;
+    fragColor = (tex * (1.-inter)) + (vec4(line_color) * inter);
+    // fragColor = (vec4(0) * (1.-inter)) + (vec4(line_color) * inter);
 
-    fragColor = pinLight(fragColor, vec4(line_color));
+    fragColor = pinLight(tex, vec4(line_color));
+    // fragColor = mix(fragColor, tex, .5);
 }
 
 void main( void ){vec4 color = vec4(0.0,0.0,0.0,1.0); mainImage(color, gl_FragCoord.xy);color.w = 1.0;FragColor = color;}
