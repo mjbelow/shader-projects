a875686729000215ecf75f565ced8dec068029b2
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2021-10-24 20:54:58 -0400

    uv-testing

--- image.glsl
+++ image.glsl
@@ -14,84 +14,39 @@ uniform vec2 iResolution;
 uniform float iTime;
 
 
-// The MIT License
-// Copyright © 2020 Inigo Quilez
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-
-// Signed distance and circle
-
-// List of some other 2D distances:
-//
-// Circle:               https://www.shadertoy.com/view/3ltSW2
-// Segment:              https://www.shadertoy.com/view/3tdSDj
-// Triangle:             https://www.shadertoy.com/view/XsXSz4
-// Isosceles Triangle:   https://www.shadertoy.com/view/MldcD7
-// Regular Triangle:     https://www.shadertoy.com/view/Xl2yDW
-// Regular Pentagon:     https://www.shadertoy.com/view/llVyWW
-// Regular Octogon:      https://www.shadertoy.com/view/llGfDG
-// Rounded Rectangle:    https://www.shadertoy.com/view/4llXD7
-// Rhombus:              https://www.shadertoy.com/view/XdXcRB
-// Trapezoid:            https://www.shadertoy.com/view/MlycD3
-// Polygon:              https://www.shadertoy.com/view/wdBXRW
-// Hexagram:             https://www.shadertoy.com/view/tt23RR
-// Regular Star:         https://www.shadertoy.com/view/3tSGDy
-// Star5:                https://www.shadertoy.com/view/wlcGzB
-// Ellipse 1:            https://www.shadertoy.com/view/4sS3zz
-// Ellipse 2:            https://www.shadertoy.com/view/4lsXDN
-// Quadratic Bezier:     https://www.shadertoy.com/view/MlKcDD
-// Uneven Capsule:       https://www.shadertoy.com/view/4lcBWn
-// Vesica:               https://www.shadertoy.com/view/XtVfRW
-// Cross:                https://www.shadertoy.com/view/XtGfzw
-// Pie:                  https://www.shadertoy.com/view/3l23RK
-// Arc:                  https://www.shadertoy.com/view/wl23RK
-// Horseshoe:            https://www.shadertoy.com/view/WlSGW1
-// Parabola:             https://www.shadertoy.com/view/ws3GD7
-// Parabola Segment:     https://www.shadertoy.com/view/3lSczz
-// Rounded X:            https://www.shadertoy.com/view/3dKSDc
-// Joint:                https://www.shadertoy.com/view/WldGWM
-// Simple Egg:           https://www.shadertoy.com/view/Wdjfz3
-//
-// and many more here:   http://www.iquilezles.org/www/articles/distfunctions2d/distfunctions2d.htm
-
-
-   
-float udSegment( in vec2 p, in vec2 a, in vec2 b, float th )
-{
-    th = 0.;
-    float l = length(b-a);
-    vec2  d = (b-a)/l;
-    vec2  q = (p-(a+b)*0.5);
-          q = mat2(d.x,-d.y,d.y,d.x)*q;
-          q = abs(q)-vec2(l,th)*0.5;
-          
-    return max(q.x,q.y);
-    return length(max(q,0.0)) + min(max(q.x,q.y),0.0);    
-}
-
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-	vec2 p = (2.0*fragCoord-iResolution.xy)/iResolution.y;
-    //p *= 1.4;
+    // Normalized pixel coordinates (from 0 to 1)
+    //vec2 uv = (fragCoord*2.-1.*iResolution.xy)/iResolution.x;
+    //vec2 uv = (fragCoord*1.-.5*iResolution.xy)/iResolution.x;
+    vec2 uv = (fragCoord)/iResolution.x;
+
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+
+    uv *= 5.;
     
-    vec2 v1 = vec2(0);//cos( iTime + vec2(0.0,2.00) + 0.0 );
-	vec2 v2 = vec2(.001,0);//cos( iTime + vec2(0.0,1.50) + 1.5 );
-    float th = 0.1*(0.5+0.5*sin(iTime*1.1));
+    //uv = floor(uv);
     
-	float d = udSegment( p, v1, v2, 2. ) ;
+    //uv /= 10.;
     
-    vec3 col = vec3(1.0) - sign(d)*vec3(0.1,0.4,0.7);
-	col *= 1.0 - exp(-3.0*abs(d));
-	col *= 0.8 + 0.2*cos(120.0*d);
-	col = mix( col, vec3(1.0), 1.0-smoothstep(0.0,0.015,abs(d)) );
+    int m = int(uv.x);
     
-    d *= 10.;
-    d = floor(d);
-    d /= 10.;
+    uv = fract(uv);
     
-    col = vec3(d);
+    uv.y *= 5.;
+    uv.y = floor(uv.y);
+    uv.y /= 5.;
+
+    // Output to screen
     
-	fragColor = vec4(col,1.0);
+    if(m % 2 == 0)
+        fragColor = vec4(uv.x,uv.y,uv.y,1.0);
+    else
+        fragColor = vec4(uv.y,uv.x,uv.x,1.0);
+    
+    if(uv.y==.6)
+        fragColor = vec4(0);
 }
 
 void main( void ){vec4 color = vec4(0.0,0.0,0.0,1.0); mainImage(color, gl_FragCoord.xy);color.w = 1.0;FragColor = color;}
