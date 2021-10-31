7b3959ea0a46068d66f9794f95805a6ca7fa7ce9
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2021-10-31 15:42:11 -0400

    Add gradient map function

--- image.glsl
+++ image.glsl
@@ -49,6 +49,51 @@ uniform int top_layer_option;
 uniform int bottom_layer_option;
 uniform int transition_layer_option;
 
+// channels
+const vec3 rgb_ratio = vec3(0.299 , 0.587 , 0.114);
+// rgb_ratio = vec3(.33);
+
+vec3 gradient_map_rainbow(vec3 col)
+{
+  float f = dot(col, rgb_ratio);
+  f *= 6;
+  int i = int(floor(f));
+  f = fract(f);
+
+  vec3 color_a;
+  vec3 color_b;
+
+  switch (i)
+  {
+  case 0:
+      color_a = vec3(1,0,0);
+      color_b = vec3(1,1,0);
+      break;
+  case 1:
+      color_a = vec3(1,1,0);
+      color_b = vec3(0,1,0);
+      break;
+  case 2:
+      color_a = vec3(0,1,0);
+      color_b = vec3(0,1,1);
+      break;
+  case 3:
+      color_a = vec3(0,1,1);
+      color_b = vec3(0,0,1);
+      break;
+  case 4:
+      color_a = vec3(0,0,1);
+      color_b = vec3(1,0,1);
+      break;
+  case 5:
+      color_a = vec3(1,0,1);
+      color_b = vec3(1,0,0);
+      break;
+  }
+
+  return mix(color_a, color_b, f);
+}
+
 #define S(a, b, c) smoothstep(a, b, c)
 // #define SL(a, b, c) smoothstep(1.-a, 1.-b, c)
 // #define S(a, b, c) step(a,c)
@@ -61,14 +106,9 @@ uniform int transition_layer_option;
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     // Normalized pixel coordinates (from 0 to 1)
-    // fragCoord.y = iResolution.y - fragCoord.y;
     vec2 uv = fragCoord/iResolution.xy;
 
-    // Time varying pixel color
-    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
-
-    // Output to screen
-    fragColor = texture(iChannel1, uv);
+    vec3 col;
     
     vec4 layers[4];
     layers[0] = texture(iChannel0, uv);
@@ -77,13 +117,10 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     layers[3] = vec4(uv.x,0,0,1);
 
 
-    vec4 top_layer = layers[top_layer_option];
-    vec4 bottom_layer = layers[bottom_layer_option];
-    vec4 transition_layer = layers[transition_layer_option];
-    
-    // channels
-    vec3 rgb_ratio = vec3(0.299 , 0.587 , 0.114);
-    // rgb_ratio = vec3(.33);
+    vec3 top_layer = layers[top_layer_option].rgb;
+    vec3 bottom_layer = layers[bottom_layer_option].rgb;
+    vec3 transition_layer = layers[transition_layer_option].rgb;
+
     
     float top_gray = dot(top_layer.rgb, rgb_ratio);
     float top_red = top_layer.r;
@@ -94,9 +131,6 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     float bottom_red = bottom_layer.r;
     float bottom_green = bottom_layer.g;
     float bottom_blue = bottom_layer.b;
-    
-    // gray_light_top_1 = 1.-gray_light_top_1;
-    // gray_light_top_2 = 1.-gray_light_top_2;
     
     float top_mix = 0.;
 
@@ -109,7 +143,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     top_mix = mix(1., top_mix, S(blue_dark_top_1 - EPSILON, max(blue_dark_top_1,blue_dark_top_2), top_blue));
     top_mix = mix(top_mix, 1., S(min(blue_light_top_1,blue_light_top_2) - EPSILON, blue_light_top_1, top_blue));
 
-    fragColor = mix(top_layer, transition_layer, top_mix);
+    col = mix(top_layer, transition_layer, top_mix);
     
     float bottom_mix = 1.;
 
@@ -122,12 +156,9 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     bottom_mix = mix(0., bottom_mix, S(blue_dark_bottom_1 - EPSILON, max(blue_dark_bottom_1,blue_dark_bottom_2), bottom_blue));
     bottom_mix = mix(bottom_mix, 0., S(min(blue_light_bottom_1,blue_light_bottom_2) - EPSILON, blue_light_bottom_1, bottom_blue));
     
-    // fragColor = vec4(bottom_mix);
-    fragColor = mix(transition_layer, fragColor, bottom_mix);
+    col = mix(transition_layer, col, bottom_mix);
 
-    // if(t == top_layer_option)
-      // fragColor = vec4(0,1,0,1);
-    // fragColor = vec4(gray_light_top_1,0,0,1);
+    fragColor = vec4(col, 1);
 }
 
 void main( void ){vec4 color = vec4(0.0,0.0,0.0,1.0); mainImage(color, gl_FragCoord.xy);color.w = 1.0;FragColor = color;}
